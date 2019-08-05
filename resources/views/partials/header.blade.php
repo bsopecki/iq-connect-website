78e0ecfdc5e519ae4df2174885c2a7fbf7841047
Bartlomiej <b.sopecki@gmail.com>
2019-08-05 13:45:09 +0200

    finish HTML structure and add basic styles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,27 @@
 <header class="header">
-  <a href=""><img src="@asset('images/LOGO.png')" alt=""></a>
+  <a class="header__logo" href="">
+    <img src="@asset('images/LOGO.png')" alt="IQ CONNECT logo">
+  </a>
   <div class="container">
-    <nav class="header__nav" data-nav>
+    <div class="header__wrapper">
+      <nav class="header__nav" data-nav>
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'header__menu']) !!}
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'menu']) !!}
       @endif
-    </nav>
+      </nav>
+      <div class="header__contact">
+        <p class="text text--minor">
+        Zadzwoń 
+          <a href="#" class="link">
+          513 529 029
+          </a>
+        </p>
+        <a href="#" class="button">
+          <p class="button__text">
+        Kontakt.
+        </p>
+        </a>
+      </div>
+    </div>
   </div>
-  <div class="phone-number">
-    <p class="call">Zadzwoń</p>
-    <span class="number">513 529 029</span>
-  </div>
-  <button class="btn_contact">Kontakt.</button>
 </header>
-
