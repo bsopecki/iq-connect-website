5ad06b446972e34e7d4e31e2644a62160b83defd
Bartlomiej <b.sopecki@gmail.com>
2019-08-20 12:44:31 +0200

    add logo images and header styles

--- resources/views/layouts/components/about-us.blade.php
+++ resources/views/layouts/components/about-us.blade.php
@@ -1,7 +1,104 @@
-<section class="about-us">
+<section class="section">
     <div class="container">
-        <header class="about-us__header">
-            <h1 class="title"><span class="about-us__subtitle">co robimy</span></h1>
+        <header class="section__header">
+            <h2 class="section__title title--upper">
+                <span class="section__subtitle title--upper"
+                >co robimy
+                </span>
+                <br>
+              looking forward
+            </h2>
+            <p class="section__content text--center">
+                Podejmujemy ciekawe wyzwania, za każdym razem biorąc pod uwagę oczekiwania i potrzeby naszych klientów.<br>Sprawdźcie szczegóły naszej bogatej oferty usług.
+            </p>
         </header>
+        <div class="section__wrapper">
+            <ul class="posts">
+                <li class="posts__item">
+                    <article class="posts__article">
+                        <img src="@asset('images/posts_logos/023-target-1.png')" alt="website logo" class="posts__logo">
+                        <h2 class="posts__intro">
+                            Strony / sklepy internetowe
+                        </h2>
+                        <p class="posts__summary">
+                            Nasz zespół webdeweloperów, wsparty pracą zespołu grafików, zajmuje się projektowaniem graficznym oraz tworzeniem stron i sklepów internetowych.
+                        </p>
+                        <a href="#" class="posts__link menu-link--more">
+                            więcej o stronach internetowych
+                        </a>
+                    </article>
+                </li>
+                <li class="posts__item">
+                    <article class="posts__article">
+                        <img src="@asset('images/posts_logos/028-mobile-app.png')" alt="moblie aps logo" class="posts__logo">
+                        <h2 class="posts__intro">
+                            Aplikacje mobilne
+                        </h2>
+                        <p class="posts__summary">
+                            Aplikacje mobilne zdobyły rynek, a ich szeroka gama zastosowań dotyczy niemal każdej branży. Tworzymy wysokiej jakości projekty na różnego rodzaju systemy operacyjne.
+                        </p>
+                        <a href="#" class="posts__link menu-link--more">
+                                więcej o aplikacjach
+                            </a>
+                    </article>
+                </li>
+                <li class="posts__item">
+                    <article class="posts__article">
+                        <img src="@asset('images/posts_logos/016-web-management.png')" alt="systems logo" class="posts__logo">
+                        <h2 class="posts__intro">
+                            Systemy dedykowane
+                        </h2>
+                        <p class="posts__summary">
+                            Pozwalają zarządzać dużymi sieciami czy aplikacjami i szybko rozwiązywać problemy czy wprowadzać zmiany w systemach.
+                        </p>
+                        <a href="#" class="posts__link menu-link--more">
+                            więcej o systemch dedykowanych
+                        </a>
+                    </article>
+                </li>
+                <li class="posts__item">
+                    <article class="posts__article">
+                        <img src="@asset('images/posts_logos/004-report.png')" alt="CRM's logo" class="posts__logo">
+                        <h2 class="posts__intro">
+                            CRM
+                        </h2>
+                        <p class="posts__summary">
+                            Oprogramowanie CRM to technologia do skutecznego Zarządzania Relacjami z Klientami. System CRM jest niezbędny sprawnego funkcjonowania przedsiębiorstwa.
+                        </p>
+                        <a href="#" class="posts__link menu-link--more">
+                            więcej o stronach internetowych
+                        </a>
+                    </article>
+                </li>
+                <li class="posts__item">
+                    <article class="posts__article">
+                        <img src="@asset('images/posts_logos/022-digital-campaign.png')" alt="Marketing logo" class="posts__logo">
+                        <h2 class="posts__intro">
+                            Conent Marketing
+                        </h2>
+                        <p class="posts__summary">
+                            Aplikacje mobilne zdobyły rynek, a ich szeroka gama zastosowań dotyczy niemal każdej branży. Tworzymy wysokiej jakości projekty na różnego rodzaju systemy operacyjne.
+                        </p>
+                        <a href="#" class="posts__link menu-link--more">
+                            więcej o aplikacjach
+                        </a>
+                    </article>
+                </li>
+                <li class="posts__item">
+                    <article class="posts__article">
+                        <img src="@asset('images/posts_logos/024-ads.png')" alt="SEO logo" class="posts__logo">
+                        <h2 class="posts__intro">
+                            Pozycjonowanie / Reklama AdWords
+                        </h2>
+                        <p class="posts__summary">
+                            Pozwalają zarządzać dużymi sieciami czy aplikacjami i szybko rozwiązywać problemy czy wprowadzać zmiany w systemach.
+                        </p>
+                        <a href="#" class="posts__link menu-link--more">
+                            więcej o systemach dedykowanych
+                        </a>
+                    </article>
+                </li>
+            </ul>
+        </div>
     </div>
 </section>
