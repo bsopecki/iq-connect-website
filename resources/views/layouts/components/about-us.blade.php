31c994cbbe8bb901917a1eb3d5afd172a600617e
Bartlomiej <b.sopecki@gmail.com>
2019-08-21 14:06:43 +0200

    finish about-us section

--- resources/views/layouts/components/about-us.blade.php
+++ resources/views/layouts/components/about-us.blade.php
@@ -16,84 +16,84 @@
             <ul class="posts">
                 <li class="posts__item">
                     <article class="posts__article">
-                        <img src="@asset('images/posts_logos/023-target-1.png')" alt="website logo" class="posts__logo">
+                        <img class="posts__logo" src="@asset('images/posts_logos/023-target-1.png')" alt="website logo" class="posts__logo">
                         <h2 class="posts__intro">
-                            Strony / sklepy internetowe
+                            Strony/sklepy internetowe
                         </h2>
                         <p class="posts__summary">
                             Nasz zespół webdeweloperów, wsparty pracą zespołu grafików, zajmuje się projektowaniem graficznym oraz tworzeniem stron i sklepów internetowych.
                         </p>
-                        <a href="#" class="posts__link menu-link--more">
+                        <a href="#" class="posts__link text--more">
                             więcej o stronach internetowych
                         </a>
                     </article>
                 </li>
                 <li class="posts__item">
                     <article class="posts__article">
-                        <img src="@asset('images/posts_logos/028-mobile-app.png')" alt="moblie aps logo" class="posts__logo">
+                        <img class="posts__logo" src="@asset('images/posts_logos/028-mobile-app.png')" alt="moblie aps logo" class="posts__logo">
                         <h2 class="posts__intro">
                             Aplikacje mobilne
                         </h2>
                         <p class="posts__summary">
                             Aplikacje mobilne zdobyły rynek, a ich szeroka gama zastosowań dotyczy niemal każdej branży. Tworzymy wysokiej jakości projekty na różnego rodzaju systemy operacyjne.
                         </p>
-                        <a href="#" class="posts__link menu-link--more">
+                        <a href="#" class="posts__link text--more">
                                 więcej o aplikacjach
                             </a>
                     </article>
                 </li>
                 <li class="posts__item">
                     <article class="posts__article">
-                        <img src="@asset('images/posts_logos/016-web-management.png')" alt="systems logo" class="posts__logo">
+                        <img class="posts__logo" src="@asset('images/posts_logos/016-web-management.png')" alt="systems logo" class="posts__logo">
                         <h2 class="posts__intro">
                             Systemy dedykowane
                         </h2>
                         <p class="posts__summary">
                             Pozwalają zarządzać dużymi sieciami czy aplikacjami i szybko rozwiązywać problemy czy wprowadzać zmiany w systemach.
                         </p>
-                        <a href="#" class="posts__link menu-link--more">
+                        <a href="#" class="posts__link text--more">
                             więcej o systemch dedykowanych
                         </a>
                     </article>
                 </li>
                 <li class="posts__item">
                     <article class="posts__article">
-                        <img src="@asset('images/posts_logos/004-report.png')" alt="CRM's logo" class="posts__logo">
+                        <img class="posts__logo" src="@asset('images/posts_logos/004-report.png')" alt="CRM's logo" class="posts__logo">
                         <h2 class="posts__intro">
                             CRM
                         </h2>
                         <p class="posts__summary">
                             Oprogramowanie CRM to technologia do skutecznego Zarządzania Relacjami z Klientami. System CRM jest niezbędny sprawnego funkcjonowania przedsiębiorstwa.
                         </p>
-                        <a href="#" class="posts__link menu-link--more">
+                        <a href="#" class="posts__link text--more">
                             więcej o stronach internetowych
                         </a>
                     </article>
                 </li>
                 <li class="posts__item">
                     <article class="posts__article">
-                        <img src="@asset('images/posts_logos/022-digital-campaign.png')" alt="Marketing logo" class="posts__logo">
+                        <img class="posts__logo" src="@asset('images/posts_logos/022-digital-campaign.png')" alt="Marketing logo" class="posts__logo">
                         <h2 class="posts__intro">
                             Conent Marketing
                         </h2>
                         <p class="posts__summary">
                             Aplikacje mobilne zdobyły rynek, a ich szeroka gama zastosowań dotyczy niemal każdej branży. Tworzymy wysokiej jakości projekty na różnego rodzaju systemy operacyjne.
                         </p>
-                        <a href="#" class="posts__link menu-link--more">
+                        <a href="#" class="posts__link text--more">
                             więcej o aplikacjach
                         </a>
                     </article>
                 </li>
                 <li class="posts__item">
                     <article class="posts__article">
-                        <img src="@asset('images/posts_logos/024-ads.png')" alt="SEO logo" class="posts__logo">
+                        <img class="posts__logo" src="@asset('images/posts_logos/024-ads.png')" alt="SEO logo" class="posts__logo">
                         <h2 class="posts__intro">
                             Pozycjonowanie / Reklama AdWords
                         </h2>
                         <p class="posts__summary">
                             Pozwalają zarządzać dużymi sieciami czy aplikacjami i szybko rozwiązywać problemy czy wprowadzać zmiany w systemach.
                         </p>
-                        <a href="#" class="posts__link menu-link--more">
+                        <a href="#" class="posts__link text--more">
                             więcej o systemach dedykowanych
                         </a>
                     </article>
